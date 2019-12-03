55c7ea360fa983cb54eb5404f5a10d2a671555ef
samspurs5 <36265555+samspurs5@users.noreply.github.com>
2019-12-03 11:46:42 +0000

    Add files via upload
    Updated to use powershell functions to reduce code duplication and improve maintainability

--- revision.ps1
+++ revision.ps1
@@ -1,3 +1,19 @@
+function buildPath{
+	Param ($Path,
+	$PathName, 
+	$Name
+	)
+	If  (-NOT (Test-Path $PathName)){
+		Write-Host "creating ${name} folder"
+		Write-Host $Path
+		Write-Host $PathName
+		Write-Host $Name
+		New-Item -Path $Path -Name $name -ItemType "directory"
+	}
+	Else{
+		Write-Host "${Name} folder exists"
+	}
+}
 
 $Credentials = Get-Credential
 $user =  $Credentials.Username
@@ -11,34 +27,13 @@ $repeat = "Y"
 
 while ($repeat -eq "Y"){
 	$ModuleCode = Read-Host -Prompt "Input module code"
-	If  (-NOT (Test-Path .\revision)){
-		Write-Host "creating revision folder"
-		New-Item -Path . -Name "revision" -ItemType "directory"
-	}
-	Else{
-		Write-Host "revision folder exists"
-	}
-
-	If  (-NOT (Test-Path .\revision\$ModuleCode)){
-		Write-Host "creating module:"$ModuleCode" folder"
-		New-Item -Path ".\revision" -Name "$ModuleCode" -ItemType "directory"
-	}
-	Else{
-		Write-Host "module:"$ModuleCode" folder exists"
-	}
-
-	If  (-NOT (Test-Path .\revision\$ModuleCode\past-papers)){
-		Write-Host "creating past-papers folder"
-		New-Item -Path .\revision\$ModuleCode -Name "past-papers" -ItemType "directory"
-	}
-	Else{
-		Write-Host  "past-papers folder exists"
-	}
+	buildPath -Path . -PathName .\revision -Name "revision"
+	buildPath -Path .\revision -PathName .\revision\$ModuleCode -Name $ModuleCode
+	buildPath -Path .\revision\$ModuleCode -PathName .\revision\$ModuleCode\past-papers -Name "past-papers"
 	for($j=1 ; $j -le 2; $j++){
 		for($i=15 ; $i -le 18; $i++){
-
 			$PaperName = $ModuleCode +"-"+ $i +".pdf"
-			$PaperPath = ".\revision\${ModuleCode}\past-papers\${j}${PaperName}"
+			$PaperPath = ".\revision\${ModuleCode}\past-papers\${ModuleCode}-${j}-${i}.pdf"
 			Invoke-WebRequest -uri "https://www.aber.ac.uk/en/media/departmental/examinations/pastpapers/pdf/compsci/sem${j}-${i}/${ModuleCode}-${i}.pdf" -Headers $headers -OutFile $PaperPath
 			if ((Get-Item($PaperPath)).length -eq 0){
 				Remove-Item $PaperPath
